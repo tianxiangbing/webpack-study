f465efc50932272cc140d86a0f366d239ca9ffc4
tianxiangbing <55342775@qq.com>
2016-04-22 11:58:24 +0800

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+webpack = require('webpack');
+WebpackDevServer = require('webpack-dev-server');
+var config = require("./webpack.config.js");
+config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+
+});
+server.listen(8080);
