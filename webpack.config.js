cbd649fd0abc33ae4e620febdeb40dca0366998f
tianxiangbing <55342775@qq.com>
2016-04-22 18:00:29 +0800

    react dev_server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 var path = require("path");
 module.exports = {
     entry: {
-        app: ["./app/main.js"]
+        app: ["./app/app.jsx"]
     },
     output: {
         path: path.resolve(__dirname, "build"),
         publicPath: "/assets/",
-        filename: "bundle.js"
+        filename: "[name].js"
+    },
+    resolve: {
+        modulesDirectories: ["web_modules", "node_modules", "bower_components"],
+        extensions: ['', '.js', '.jsx','css']
+    },
+    module:{
+        loaders:[{
+            jsx:/\.(js|jsx)$/,
+            exclude: /(node_modules|bower_components)/,
+            loader:'babel',
+            query:{
+                presets:['es2015','react']
+            }
+        },
+            {
+                test:/\.scss$/,
+                loaders:['style','css','sass']
+            }
+        ]
     }
 };
