06f8503d623a177ca972a3b93c7f91b4cdb95b6d
田想兵 <tianxb@shinemo.com>
2016-05-27 11:45:41 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 var path = require("path");
 var webpack = require('webpack');
-
-console.log(process.env.NODE_ENV)
-var TEST = process.env.NODE_ENV === "test" ;
-var filename= TEST?"[name]":"[chunkhash:8].[name]";
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+console.log("+++++++++++"+process.env.NODE_ENV+"***********")
+var TEST = process.env.NODE_ENV == "test" ;
+console.log(TEST)
+var filename= TEST?"[name]":"[chunkhash:8].[name]";
+console.log(filename)
 var extractCSS = new ExtractTextPlugin('stylesheets/'+filename+'.css');
-//var ignoreFiles = new webpack.IgnorePlugin(new RegExp("^(jquery|react|react-dom)$"));//排除文件，但不能再打包这个文件了
+//var ignoreFiles = new webpack.IgnorePlugin(new RegExp("^(jquery|react|react-dom)$"));
 
 //动态创建html
 var HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -15,17 +16,17 @@ var htmlPlugin = new HtmlWebpackPlugin({
     filename: '../index-publish.html',
     template:"template.html"
 });
-module.exports = {
-    //devtool: "source-map",
+
+var config ={
     entry: {
         app: ["./app/app.jsx"],
-        vendor:["react","react-dom"]//单独打包react和react-dom进vendor
+        vendor:["react","react-dom",'whatwg-fetch','react-router']
     },
     output: {
         path: path.resolve(__dirname, "build"),
-        //publicPath: "/assets/",
-        publicPath:"build",
-        filename: filename+".js"//文件名取hash8位，默认16位
+        //publicPath: "/data/assets/build/",
+        publicPath: "build/",
+        filename: filename+".js"
     },
     resolve: {
         modulesDirectories: ["web_modules", "node_modules", "bower_components"],
@@ -37,22 +38,34 @@ module.exports = {
             exclude: /(node_modules|bower_components)/,
             loader:'babel',
             query:{
-                presets:['es2015','react']//react使用es6的写法
+                presets:['es2015','react']
             }
         },
             {
                 test:/\.scss$/,
-                loader:extractCSS.extract('style-loader','css?modules&sourceMap!sass')//css?modules可以js里引用css，但会导致css的名称变成hash
+                loader:extractCSS.extract('style-loader','css?sourceMap!sass?includePaths[]='+ path.resolve(__dirname, 'app/scss'))
             },
             {
                 test:/\.html$/,loader:"html-loader"
-            }
+            },
+            { test: /\.png$/, loader: "file-loader?name=[hash:8].[name].[ext]" }
         ]
     },
     plugins: [
-        extractCSS,//输出样式文件
+        new webpack.DefinePlugin({
+          "process.env": {
+            NODE_ENV: JSON.stringify("production")
+          }
+        }),
+        extractCSS,
         //ignoreFiles
-        new webpack.optimize.CommonsChunkPlugin("vendor", "base.js")//输出vendor名字为base.js
+        new webpack.optimize.CommonsChunkPlugin("vendor", "base.js")
         ,htmlPlugin
     ]
 };
+if(process.env.NODE_ENV == "test"){
+    config.devtool= "source-map";
+    config.output.publicPath="/";
+}
+console.log(config)
+module.exports = config
